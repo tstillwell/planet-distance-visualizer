913016bb7f0d8bf1fbba71d756f1370b3b479320
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-07 22:32:41 -0600

    feat: Add 'near' and 'far' vals for zoom limits

--- planets.js
+++ planets.js
@@ -1,4 +1,6 @@
 function loadScene(){
+    const near = 1;
+    const far = 7.47e12;
     const solarRadius = 695700;
     const mercuryRadius = 2440;
     var scene = new THREE.Scene();
@@ -7,15 +9,15 @@ function loadScene(){
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    var sunGeometry = new THREE.SphereGeometry( 10, 32, 32 );
+    var sunGeometry = new THREE.SphereGeometry( 100, 32, 32 );
     var sunMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
     var sun = new THREE.Mesh( sunGeometry, sunMaterial );
-    var mercuryGeometry = new THREE.SphereGeometry( 1, 32, 32);
+    var mercuryGeometry = new THREE.SphereGeometry( 10, 32, 32);
     var mercuryMaterial = new THREE.MeshBasicMaterial( {color: 0x97979F} );
     var mercury = new THREE.Mesh (mercuryGeometry, mercuryMaterial);
     scene.add( sun );
     scene.add( mercury );
-    mercury.position.set( 100, 0, 0)
+    mercury.position.set( 1000, 0, 0)
     camera.position.z = 100;
     var animate = function () {
         requestAnimationFrame( animate );
