d3701f282053f0d1a6262054fb68774b9b982633
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-08 22:37:09 -0600

    feat: Use km values for distance and body

--- planets.js
+++ planets.js
@@ -1,5 +1,5 @@
 function loadScene(){
-    const near = 10000;
+    const near = 100;
     const far = 1e10;
     const solarRadius = 695700;
     const mercuryRadius = 2440;
@@ -15,16 +15,16 @@ function loadScene(){
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    var sunGeometry = new THREE.SphereGeometry( 1000, 32, 32 );
+    var sunGeometry = new THREE.SphereGeometry( solarRadius * 2, 32, 32 );
     var sunMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
     var sun = new THREE.Mesh( sunGeometry, sunMaterial );
-    var mercuryGeometry = new THREE.SphereGeometry( 10, 32, 32);
+    var mercuryGeometry = new THREE.SphereGeometry( mercuryRadius * 20, 32, 32);
     var mercuryMaterial = new THREE.MeshBasicMaterial( {color: 0x97979F} );
     var mercury = new THREE.Mesh (mercuryGeometry, mercuryMaterial);
     scene.add( sun );
     scene.add( mercury );
-    mercury.position.set( 10000, 0, 0)
-    camera.position.z = 25000;
+    mercury.position.set( mercuryDistance, 0, 0)
+    camera.position.z = 95000000;
     var animate = function () {
         requestAnimationFrame( animate );
         renderer.render( scene, camera );
