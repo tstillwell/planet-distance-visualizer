3b82c3965f4ac5842cbda28b70f260a2201564ad
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-10 21:38:06 -0600

    feat: Add mars mesh to model

--- planets.js
+++ planets.js
@@ -32,6 +32,7 @@ function loadScene(){
     var earth = new THREE.Mesh (earthGeometry, earthMaterial);
     var marsGeometry = new THREE.SphereGeometry( marsRadius * 2, 32, 32);
     var marsMaterial = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
+    var mars = new THREE.Mesh (marsGeometry, marsMaterial);
     scene.add( sun );
     scene.add( mercury );
     scene.add( venus );
