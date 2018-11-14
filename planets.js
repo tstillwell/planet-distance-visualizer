ecc32cfd96eb51f64fb8abfea03056f872703c18
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-13 19:07:59 -0600

    feat: Add uranus distance const to model

--- planets.js
+++ planets.js
@@ -14,6 +14,7 @@ function loadScene(){
     const jupiterDistance = 483800000;
     const saturnRadius = 36184;
     const saturnDistance = 1434000000;
+    const uranusRadius = 25362;
     var scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, near, far );
     controls = new THREE.OrbitControls(camera);
