661afa754d18647ff972865cff7d1bb5e90f261d
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-18 22:07:45 -0500

    style: Make neptune color caps

--- planets.js
+++ planets.js
@@ -49,7 +49,7 @@ function loadScene(){
     var uranusMaterial = new THREE.MeshBasicMaterial( {color: 0x8DA1AC} );
     var uranus = new THREE.Mesh (uranusGeometry, uranusMaterial);
     var neptuneGeometry = new THREE.SphereGeometry( neptuneRadius * 2, 32, 32);
-    var neptuneMaterial = new THREE.MeshBasicMaterial( {color: 0x44667f} );
+    var neptuneMaterial = new THREE.MeshBasicMaterial( {color: 0x44667F} );
     var neptune = new THREE.Mesh (neptuneGeometry, neptuneMaterial);
     scene.add(sun);
     scene.add(mercury);
