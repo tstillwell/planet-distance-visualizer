e738fc5adff19afa468743b8f8fd43439822e050
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-02 11:33:17 -0700

    refactor: Remove uranusRadius var

--- planets.js
+++ planets.js
@@ -1,5 +1,4 @@
 function loadScene(){
-    const uranusRadius = 25362;
     const neptuneRadius = 24622;
     let scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 1e10 );
@@ -14,7 +13,7 @@ function loadScene(){
     let mars = planetMeshFactory({radius: 3390, texture: 0xFF0000});
     let jupiter = planetMeshFactory({radius: 69911, texture: 0xE3DCCB});
     let saturn = planetMeshFactory({radius: 36184, texture: 0xE5E3DB});
-    let uranus = planetMeshFactory({radius: uranusRadius, texture: 0x8DA1AC});
+    let uranus = planetMeshFactory({radius: 25362, texture: 0x8DA1AC});
     let neptune = planetMeshFactory({radius: neptuneRadius, texture: 0x44667F});
     let model_data = [
         {id: sun, distance: 0},
