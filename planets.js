3bf5b3871604b26f66022d8bcd463b45848f17ee
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-07 12:45:10 -0700

    style: Use 'let' keyword for camera var in loadScene

--- planets.js
+++ planets.js
@@ -1,6 +1,6 @@
 function loadScene(){
     let scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 1e15 );
+    let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 1e15 );
     controls = new THREE.OrbitControls(camera);
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
