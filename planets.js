b93a876de9902014a7cf99ef25e18a955a559b47
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-12 17:58:26 -0700

    feat: Enable logarithmicDepthBuffer for webgl renderer

--- planets.js
+++ planets.js
@@ -2,7 +2,7 @@
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 1e15 );
     let controls = new THREE.OrbitControls(camera);
-    let renderer = new THREE.WebGLRenderer({antialias: true});
+    let renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true});
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     let sun = planetMesh({radius: 695700, texture: 0xFFFF00});
