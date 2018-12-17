2380d36b153fe4c0e342d1a9603377ab477b2dc0
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-16 19:06:00 -0700

    feat: Add name property for jupiter mesh

--- planets.js
+++ planets.js
@@ -11,7 +11,7 @@
     let venus = planetMesh({radius: 6050, texture: 0xE5E3DB, name: "venus"});
     let earth = planetMesh({radius: 6371, texture: 0x00FF00, name: "earth"});
     let mars = planetMesh({radius: 3390, texture: 0xFF0000, name: "mars"});
-    let jupiter = planetMesh({radius: 69911, texture: 0xE3DCCB});
+    let jupiter = planetMesh({radius: 69911, texture: 0xE3DCCB, name: "jupiter"});
     let saturn = planetMesh({radius: 36184, texture: 0xE5E3DB});
     let uranus = planetMesh({radius: 25362, texture: 0x8DA1AC});
     let neptune = planetMesh({radius: 24622, texture: 0x44667F});
