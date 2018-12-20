aa5cd9a42e73d2b850082a02c4cee323dec5ce1f
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-19 22:20:00 -0700

    feat: Add saturn scene var to addOverlayListeners

--- planets.js
+++ planets.js
@@ -76,12 +76,13 @@ function addOverlayListeners(scene){
     let earth = getBodyByName(scene, "earth");
     let mars = getBodyByName(scene, "mars");
     let jupiter = getBodyByName(scene, "jupiter");
+    let jupiter = getBodyByName(scene, "saturn");
     document.querySelector("#mercury-select").addEventListener("click", selectPlanet(mercury));
     document.querySelector("#venus-select").addEventListener("click", selectPlanet(venus));
     document.querySelector("#earth-select").addEventListener("click", selectPlanet(earth));
     document.querySelector("#mars-select").addEventListener("click", selectPlanet(mars));
     document.querySelector("#jupiter-select").addEventListener("click", selectPlanet(jupiter));
-    document.querySelector("#saturn-select").addEventListener("click", selectPlanet);
+    document.querySelector("#saturn-select").addEventListener("click", selectPlanet(saturn));
     document.querySelector("#uranus-select").addEventListener("click", selectPlanet);
     document.querySelector("#neptune-select").addEventListener("click", selectPlanet);
 }
