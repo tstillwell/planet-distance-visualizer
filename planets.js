900a31ee236e5be2961008d4183c391117ee3b9f
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-21 23:00:56 -0700

    fix: Don't call uranus-select event listener

--- planets.js
+++ planets.js
@@ -85,7 +85,7 @@ function addOverlayListeners(scene){
     document.querySelector("#mars-select").addEventListener("click", function(){selectPlanet(mars)});
     document.querySelector("#jupiter-select").addEventListener("click", function(){selectPlanet(jupiter)});
     document.querySelector("#saturn-select").addEventListener("click", function(){selectPlanet(saturn)});
-    document.querySelector("#uranus-select").addEventListener("click", selectPlanet(uranus));
+    document.querySelector("#uranus-select").addEventListener("click", function(){selectPlanet(uranus)});
     document.querySelector("#neptune-select").addEventListener("click", selectPlanet(neptune));
 }
 
