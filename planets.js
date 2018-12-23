8359c9096af5be9e0787f42ec52af60b79034067
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-23 00:46:23 -0700

    feat: Update jupiter mesh texture color

--- planets.js
+++ planets.js
@@ -13,7 +13,7 @@
     let venus = planetMesh({name: "venus", radius: 6050, texture: 0xE5E3DB});
     let earth = planetMesh({name: "earth", radius: 6371, texture: 0x00CFFF});
     let mars = planetMesh({name: "mars", radius: 3390, texture: 0xB08960});
-    let jupiter = planetMesh({name: "jupiter", radius: 69911, texture: 0xE3DCCB});
+    let jupiter = planetMesh({name: "jupiter", radius: 69911, texture: 0xAD8D74});
     let saturn = planetMesh({name: "saturn", radius: 36184, texture: 0xE5E3DB});
     let uranus = planetMesh({name: "uranus", radius: 25362, texture: 0x8DA1AC});
     let neptune = planetMesh({name: "neptune", radius: 24622, texture: 0x44667F});
