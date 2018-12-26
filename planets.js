751806ad75da7229fb890725516f51a6f6b1c4d7
tstillwell2k17 <tristanstillwell@gmail.com>
2018-12-26 02:01:51 -0700

    refactor: Use bindOverlayListener for jupiter-select

--- planets.js
+++ planets.js
@@ -86,7 +86,7 @@ function addOverlayListeners(scene){ // bind overlay buttons to planets in scene
     bindOverlayListener("#venus-select", venus);
     bindOverlayListener("#earth-select", earth);
     bindOverlayListener("#mars-select", mars);
-    document.querySelector("#jupiter-select").addEventListener("click", function(){selectPlanet(jupiter);});
+    bindOverlayListener("#jupiter-select", jupiter);
     document.querySelector("#saturn-select").addEventListener("click", function(){selectPlanet(saturn);});
     document.querySelector("#uranus-select").addEventListener("click", function(){selectPlanet(uranus);});
     document.querySelector("#neptune-select").addEventListener("click", function(){selectPlanet(neptune);});
