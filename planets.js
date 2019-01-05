974b3031d0a61ebee754fe590755666a8d72281a
tstillwell <tristanstillwell@gmail.com>
2019-01-05 15:36:53 -0700

    style: Wrap use strict pragma in single quotes

--- planets.js
+++ planets.js
@@ -1,5 +1,5 @@
 (function loadScene() {
-  "use strict";
+  'use strict';
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 1e15 );
   const controls = new THREE.OrbitControls(camera);
