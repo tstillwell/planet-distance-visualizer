e1d58a204b4fcf25a082e0967bf78652f85ca465
tstillwell <tristanstillwell@gmail.com>
2019-01-13 17:23:17 -0700

    feat: Replace let with const for venus in addOverlayListeners

--- planets.js
+++ planets.js
@@ -74,7 +74,7 @@ function positionAllInSpace(scene, planets_data){ // add all planets to model
 
 function addOverlayListeners(scene){ // add event listeners for overlay
   const mercury = getBodyByName(scene, "mercury");
-  let venus = getBodyByName(scene, "venus");
+  const venus = getBodyByName(scene, "venus");
   let earth = getBodyByName(scene, "earth");
   let mars = getBodyByName(scene, "mars");
   let jupiter = getBodyByName(scene, "jupiter");
