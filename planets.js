1e42a4ec96faee976e8a7b7210d4b3802e3a9372
tstillwell <tristanstillwell@gmail.com>
2019-01-15 17:33:34 -0700

    style: Add trailing space after opening brace in getBodyByName

--- planets.js
+++ planets.js
@@ -108,7 +108,7 @@ function selectPlanet(planet){  // fired on clicking planet in overlay
 
 function getBodyByName(scene, name){ // retrieve planet from scene by name
   const bodylist = scene.children;
-  for (body in bodylist){
+  for (body in bodylist){ 
     if (bodylist[body].name === name){
       return bodylist[body];
     }
